15cb575c3ac4fa39f9be59cdcef87be9a2676e95
Talika Bajaj <bajajtalika2001@gmail.com>
2024-07-27 17:30:38 +0530

    Update files

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const adviceId = document.querySelector('.advice-id');
 const showAdvice = document.querySelector('.show-advice');
+const generateAdvice = document.getElementById('btn');
 
 async function getAdvice(params) {
     adviceId.innerHTML = '';
     showAdvice.innerHTML = '';
-   
+
     let response = await fetch('https://api.adviceslip.com/advice');
     let data = await response.json();
     console.log(data.slip);
     let displayData = data.slip;
-    
+
     adviceId.textContent = displayData.id;
     showAdvice.textContent = `"${displayData.advice}"`;
 }
 
-getAdvice();
-
-
-const generateAdvice = document.getElementById('btn');
-generateAdvice.addEventListener('click', ()=> {
+generateAdvice.addEventListener('click', () => {
     getAdvice();
 })
